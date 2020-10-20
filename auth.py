25402c5839502df5e86114d182a34ae99024ff12
NassAir <72807860+NassAir@users.noreply.github.com>
2020-10-20 15:46:27 +0200

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,8 +1,21 @@
 
-//pas fini * not finish
+#pas fini * not finish
 import requests
 import json
 
+def xenauth():
+  
+  if os.path.isfile('key.txt'):
+    key = str(input('Enter Key: '))
+    username = str(input('Enter Pseudo/Mail : '))
+    password = str(input('Enter Password : '))
+    
+  else:
+    k = open('key.txt', 'r')
+    key = k.read()
+    username = str(input('Enter Pseudo/Mail : '))
+    password = str(input('Enter Password : '))
+  
 payload = {'login': user, 'password': passs}
 r = requests.post('https://example.com(/index.php)/api/auth', data = payload)
 
